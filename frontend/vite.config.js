1230831215d66830a3b249bd8ce78b5272a3e000
Babu <gbabu5293@gmail.com>
2025-11-29 20:37:33 +0530

    deployment process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // Use esbuild instead of terser (faster, no extra dependency)
     rollupOptions: {
       output: {
         manualChunks: {
